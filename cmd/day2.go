423ad14cb5c8a93449e215d0f8da0bb2859858c5
netr <programmatical@gmail.com>
2023-12-02 00:51:23 -0800

    add day2: part2

--- cmd/day2.go
+++ cmd/day2.go
@@ -20,8 +20,8 @@ var day2Cmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		lines := util.ReadFileIntoSlice("data/day2.txt")
 		fmt.Println("URL: https://adventofcode.com/2023/day/2")
-		fmt.Println("Part1:", playGames(lines))
-		// fmt.Println("Part2:", totalCalibration(lines, calibrateWordNums))
+		fmt.Println("Part1:", playCubeGames(lines))
+		fmt.Println("Part2:", playCubeGamesForMinimum(lines))
 	},
 }
 
@@ -42,40 +42,84 @@ func isCubePossible(color string, count int) bool {
 	}
 }
 
-func playGame(game string) (int, bool) {
-	gameSplit := strings.Split(game, ": ")
-	gameId := strings.Replace(gameSplit[0], "Game ", "", -1)
+func tryCubeGame(game string) (int, bool) {
+	id, played := splitCubeGame(game)
 
-	gamesPlayed := strings.Split(gameSplit[1], "; ")
-	for _, curGame := range gamesPlayed {
+	for _, curGame := range played {
 		cubes := strings.Split(curGame, ", ")
 		for _, curCube := range cubes {
-			cubeSplit := strings.Split(curCube, " ")
-			cubeCount, err := strconv.Atoi(cubeSplit[0])
-			if err != nil {
-				panic(err)
-			}
-
-			if !isCubePossible(cubeSplit[1], cubeCount) {
+			color, count := parseCube(curCube)
+			if !isCubePossible(color, count) {
 				return 0, false
 			}
 		}
 	}
 
+	return id, true
+}
+
+// splitCubeGame splits a game string into a game id and a slice of games played
+func splitCubeGame(game string) (int, []string) {
+	gameSplit := strings.Split(game, ": ")
+	gameId := strings.Replace(gameSplit[0], "Game ", "", -1)
 	gameIdInt, err := strconv.Atoi(gameId)
 	if err != nil {
 		panic(err)
 	}
-
-	return gameIdInt, true
+	gamesPlayed := strings.Split(gameSplit[1], "; ")
+	return gameIdInt, gamesPlayed
 }
 
-func playGames(games []string) int {
+func getCubeGameMinimums(game string) int {
+	_, played := splitCubeGame(game)
+
+	cubeMap := make(map[string]int, 3)
+	cubeMap["red"] = 0
+	cubeMap["green"] = 0
+	cubeMap["blue"] = 0
+
+	for _, curGame := range played {
+		cubes := strings.Split(curGame, ", ")
+		for _, curCube := range cubes {
+			color, count := parseCube(curCube)
+			if cubeMap[color] < count {
+				cubeMap[color] = count
+			}
+		}
+	}
+
+	power := 1
+	for _, v := range cubeMap {
+		power *= v
+	}
+	return power
+}
+
+func parseCube(cube string) (string, int) {
+	s := strings.Split(cube, " ")
+	count, err := strconv.Atoi(s[0])
+	if err != nil {
+		panic(err)
+	}
+	color := s[1]
+	return color, count
+}
+
+func playCubeGames(games []string) int {
 	total := 0
 	for _, game := range games {
-		if id, ok := playGame(game); ok {
+		if id, ok := tryCubeGame(game); ok {
 			total += id
 		}
+	}
+	return total
+}
+
+func playCubeGamesForMinimum(games []string) int {
+	total := 0
+	for _, game := range games {
+		power := getCubeGameMinimums(game)
+		total += power
 	}
 	return total
 }
--- cmd/day2_test.go
+++ cmd/day2_test.go
@@ -2,21 +2,21 @@ package cmd
 
 import "testing"
 
-func Test_ParseGame(t *testing.T) {
+func Test_TryCubeGame(t *testing.T) {
 	game := "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"
-	if id, ok := playGame(game); !ok {
+	if id, ok := tryCubeGame(game); !ok {
 		t.Errorf("game %d should be possible", id)
 	}
 }
 
-func Test_ParseGame_Larger(t *testing.T) {
+func Test_TryCubeGame_Larger(t *testing.T) {
 	game := "Game 100: 3 blue, 3 red, 6 green; 7 red, 2 green, 16 blue; 14 green, 9 red, 9 blue; 8 red, 10 green, 9 blue; 6 blue, 11 red"
-	if id, ok := playGame(game); ok {
+	if id, ok := tryCubeGame(game); ok {
 		t.Errorf("game %d should not be possible", id)
 	}
 }
 
-func Test_PlayGames(t *testing.T) {
+func Test_PlayCubeGames(t *testing.T) {
 	games := []string{
 		"Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
 		"Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue",
@@ -25,8 +25,31 @@ func Test_PlayGames(t *testing.T) {
 		"Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green",
 	}
 
-	count := playGames(games)
+	count := playCubeGames(games)
 	if count != 8 {
+		t.Errorf("count should be 8, got %d", count)
+	}
+}
+
+func Test_GetCubeGameMinimums(t *testing.T) {
+	game := "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red"
+	power := getCubeGameMinimums(game)
+	if power != 630 {
+		t.Errorf("power should be 630, got %d", power)
+	}
+}
+
+func Test_PlayCubeGamesForMinimums(t *testing.T) {
+	games := []string{
+		"Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
+		"Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue",
+		"Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red",
+		"Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red",
+		"Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green",
+	}
+
+	count := playCubeGamesForMinimum(games)
+	if count != 2286 {
 		t.Errorf("count should be 8, got %d", count)
 	}
 }
